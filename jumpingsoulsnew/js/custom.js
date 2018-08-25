32489c93be038a8b37959455d24d36b056457738
ameybhivshet <ameybhivshet26995@gmail.com>
2018-08-25 19:00:57 +0530

    1st commit

---
+++ jumpingsoulsnew/js/custom.js
@@ -0,0 +1,88 @@
+
+(function($) {
+
+  new WOW().init();
+
+  // jQuery(window).load(function() {
+  //   jQuery("#preloader").delay(100).fadeOut("slow");
+  //   jQuery("#load").delay(100).fadeOut("slow");
+  // });
+
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    }
+
+    else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+
+    $('.navbar-nav li a').on('click', function(event) {
+
+      if ($(this).is('a:not([href^="#"])') || $(this).attr("href") == '#') {
+        return;
+      }
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+
+    $('.page-scroll a').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+
+  });
+
+	var navMain = $(".navbar-collapse");
+	navMain.on("click", "a:not([data-toggle])", null, function () {
+	   navMain.collapse('hide');
+	});
+
+  $('#intro').carousel({
+        interval: 4000,
+        pause: false
+  });
+  $(".nav li").on("click", function() {
+       $(".nav li ").removeClass("active");
+       $(this).addClass("active");
+     });
+     $("#testimonial-slider").owlCarousel({
+           items:1,
+           itemsDesktop:[1000,1],
+           itemsDesktopSmall:[979,1],
+           itemsTablet:[768,1],
+           pagination:true,
+           transitionStyle:"backSlide",
+            stopOnHover : true,
+           autoPlay:true
+       });
+
+
+       // $.get("nav.html", function(data){
+       //     $("#nav-placeholder").replaceWith(data);
+       // });
+       $(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+
+$('.back-to-top').click(function(){
+  $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+  return false;
+});
+})(jQuery);
